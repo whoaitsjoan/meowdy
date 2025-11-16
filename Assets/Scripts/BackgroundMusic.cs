30f01e82524f1c59cfa2db0be10b268e105199b1
Peri <pglick@depaul.edu>
2025-11-16 15:47:43 -0600

    Fixed the cat speed and music now stays throughout

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
